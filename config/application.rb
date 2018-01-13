aa5b47489e9436b9cb024ad1dc024a91f9dee321
Misaki Shioi <shioi.mm@gmail.com>
2018-01-13 23:17:38 +0900

    config/application.rbのパスを修正

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module BootstrapSample
     config.load_defaults 5.1
 
     ### 追加
-    config.assets.paths << "/node_modules/bootstrap/fonts"
+    config.assets.precompile += %w(*.eot *.svg *.ttf *.woff)
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
