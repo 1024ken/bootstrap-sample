6455c9f538510e8db652cdb4380fde756c49952b
Misaki Shioi <shioi.mm@gmail.com>
2018-01-14 10:28:08 +0900

    config/application.rbのパスを削除

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,6 @@ module BootstrapSample
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    ### 追加
-    config.assets.precompile += %w(*.eot *.svg *.ttf *.woff)
-    config.assets.paths << "#{Rails}/vendor/assets/fonts"
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
